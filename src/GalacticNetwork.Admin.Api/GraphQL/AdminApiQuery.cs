70a8f0187a12959d2cb831d72fc7dfe15fbf1120
Andrew Godfroy <andrew.godfroy@killerrin.com>
2019-11-04 23:08:46 -0500

    Update AdminApiQuery.cs

--- src/GalacticNetwork.Admin.Api/GraphQL/AdminApiQuery.cs
+++ src/GalacticNetwork.Admin.Api/GraphQL/AdminApiQuery.cs
@@ -1,4 +1,7 @@
-﻿using GraphQL.Types;
+﻿using AutoMapper;
+using GraphQL.Types;
+using Skoruba.IdentityServer4.Admin.BusinessLogic.Identity.Services.Interfaces;
+using Skoruba.IdentityServer4.Admin.BusinessLogic.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +11,23 @@ namespace GalacticNetwork.Admin.Api.GraphQL
 {
     public class AdminApiQuery<TUserDtoKey, TRoleDtoKey> : ObjectGraphType
     {
-        public AdminApiQuery()
+        private readonly IApiResourceService _apiResourceService;
+        private readonly IClientService _clientService;
+        private readonly IIdentityResourceService _identityResourceService;
+        private readonly IPersistedGrantAspNetIdentityService _persistedGrantsService;
+        private readonly IMapper _mapper;
+
+        public AdminApiQuery(IApiResourceService apiResourceService, IClientService clientService, IIdentityResourceService identityResourceService, IPersistedGrantAspNetIdentityService persistedGrantsService, IMapper mapper)
         {
+            // Store injected dependencies
+            _apiResourceService = apiResourceService;
+            _clientService = clientService;
+            _identityResourceService = identityResourceService;
+            _persistedGrantsService = persistedGrantsService;
+            _mapper = mapper;
+
+            // Setup the Queries
+
         }
     }
 }
